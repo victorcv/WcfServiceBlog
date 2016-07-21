ec208d1f4b311f98a7f9b435ab7cbd1114604c22
victorcv <victorcorreasvillas@gmail.com>
2016-07-21 10:52:04 +0200

    Primer commit de testeo, calculadora proy sin funcionar.

---
+++ WcfServiceBlog/IService1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfServiceBlog
+{
+    //http://geeks.ms/jvalero/2010/11/16/wcf-gua-completa-de-programacin-cap-ii-mi-primer-servicio-wcf/
+
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+        
+            [OperationContract]
+            int sumar(int x, int y);
+
+            [OperationContract]
+            int restar(int x, int y);
+
+            [OperationContract]
+            int multiplicar(int x, int y);
+
+            [OperationContract]
+            decimal dividir(int x, int y);
+        
+
+        [OperationContract]
+        string GetData(int value);
+
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        // TODO: Add your service operations here
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
